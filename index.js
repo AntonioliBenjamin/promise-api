e62c3089c1236f7e8a9e55c5941ce1c784a7edca
benjamin antonioli <benantonioli@outlook.fr>
2022-10-19 12:10:29 +0200

    exercice async js

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
+require('dotenv').config();
+const userApiUrl = process.env.USER_API;
+const foodApiUrl = process.env.FOOD_API;
 const axios = require('axios');
+const { updateShorthandPropertyAssignment } = require('typescript');
 
-const foodList = axios.get('https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json');
-const userApi = axios.get('https://randomuser.me/api/');
 
+const userApi = axios.get(`${userApiUrl}/api/`);
+const foodList = axios.get(`${foodApiUrl}/learning-area/javascript/apis/fetching-data/can-store/products.json`);
 
 // todo transform this in a new async await function.
-function getUserInfosWithThen() {
+function getUserInfosWithThen() {   
+    let title = userApi.then(response => {
+        console.log(response.data.results[0].name.title)
+            let firstName = userApi.then(response => {
+                console.log(response.data.results[0].name.first)
+                    let lastName = userApi.then(response => {
+                        console.log(response.data.results[0].name.last)
+                            let food = foodList.then(response => {
+                                console.log(response.data)
+                                
+                            })
+                    })
+            })
+    })
 
 }
 
 
-function getUserInfosWithAsyncAwait() {
-
+async function getUserInfosWithAsyncAwait() {
+    const AwaituserApi = await axios.get(`${userApiUrl}/api/`);
+    const AwaitfoodList = await axios.get(`${foodApiUrl}/learning-area/javascript/apis/fetching-data/can-store/products.json`);
+    console.log(AwaituserApi.data.results[0].name.title + " " + AwaituserApi.data.results[0].name.first + " " + AwaituserApi.data.results[0].name.last);
+    console.log(AwaitfoodList.data)
 }
 
-getUserInfosWithThen();
+
+(async () => {
+    //getUserInfosWithAsyncAwait();
+    getUserInfosWithThen()
+})();
